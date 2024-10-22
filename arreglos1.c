82157089b5eee7638f659af7a79cd14d17921d74
Tlalli <tlalliortega890@gmail.com>
2024-10-21 20:33:53 -0600

    Arreglo 1 completo

--- arreglos1.c
+++ arreglos1.c
@@ -9,12 +9,23 @@ int main(){
     str2[1] = 'd';
     str2[2] = 'i';
     str2[3] = 'ó';
-    str2[5] = 's';
+    str2[4] = 's';
 
     // TODO: (1) Recorre arr mediante un índice usando un ciclo
+    for (int i = 0; i < 5; i++) {
+printf("%d ", arr[i]); 
+    }
+    printf("\n"); 
 
     // TODO: (2) Recorre str mediante un índice usando un ciclo
-    
+    for (int i = 0; i < sizeof(str) - 1; i++){
+        printf("%c", str[i]); 
+    }
+    pintf("\n"); 
 
     // TODO:(3) Recorre str2 mediante un índice usando un ciclo
+    for (int i = 0; i < 5; i++){
+        printf ("%c", str2[i]); 
+    }
+    printf("\n"); 
 }
