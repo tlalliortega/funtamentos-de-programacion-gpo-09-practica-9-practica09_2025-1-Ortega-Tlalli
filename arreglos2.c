aa00edfb62b431e9ad805cbebb7d5cc5b5573e94
Tlalli <tlalliortega890@gmail.com>
2024-10-27 23:17:56 -0600

    Arreglo dos completado

--- arreglos2.c
+++ arreglos2.c
@@ -2,10 +2,14 @@
 
 int main(){
     int arr[]= {1, 2, 3, 4, 5};
+  
     //TODO: (4) Declara un apuntador que apunte al primer elemento del arreglo
-
+  int *ptr = arr; 
 
     //TODO: (5) Recorre el arreglo utilizando notación de punteros
-
+for (int i = 0; i < 5; i++){
+    printf("%d ", *(ptr + i)); 
+}
     return 0;
 }
+// prueba 
