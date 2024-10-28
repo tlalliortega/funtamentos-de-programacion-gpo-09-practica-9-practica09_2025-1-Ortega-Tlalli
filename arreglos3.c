489a2602cc124da28d02205963a20b4aa4335215
Tlalli <tlalliortega890@gmail.com>
2024-10-27 23:49:59 -0600

    Arreglo 3 completado :)

--- arreglos3.c
+++ arreglos3.c
@@ -8,13 +8,26 @@
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
     // TODO: (6) Declara el apuntador al inicio del arreglo
-    // TODO: (7) Declara el apuntador al final del arreglo
-    
+    int *ptr = arr; 
+    int temp; 
 
+    // TODO: (7) Declara el apuntador al final del arreglo
+    printf("Arreglo original: "); 
+    for (int i = 0; i < 5; i++){
+        printf("%d ", *(ptr + i)); 
+    }
+printf("\n"); 
     // TODO: (8) Imprime el arreglo original
 
+
     // TODO: (9) Crea un ciclo que invierita el arreglo, TIP: es neceario usar un apuntador temporal
-    
+     
+     int n = sizeof(arr)/sizeof(arr[0]); 
+     for (int i = 0; i < n / 2; i++){
+        temp = arr[i]; 
+        arr[i] = arr[n - i - 1]; 
+        arr[n - i - 1] = temp; 
+     }
     // Mostrar el arreglo invertido
     printf("[ ");
     for (int i = 0; i < 5; i++) {
